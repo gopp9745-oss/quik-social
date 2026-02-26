c9f92a6481488ae71ad6a974947c8c52b872acdc
gopp9745-oss <gopp9745@gmail.com>
2026-02-26 17:24:02 +0700

    Add server/models\db.js

---
+++ server/models/db.js
@@ -0,0 +1,21 @@
+const { Low } = require('lowdb');
+const { JSONFile } = require('lowdb/node');
+const path = require('path');
+
+const file = path.join(__dirname, 'db.json');
+const adapter = new JSONFile(file);
+
+const defaultData = {
+  users: [],
+  posts: [],
+  messages: [],
+  conversations: [],
+  groups: [],
+  groupMembers: [],
+  friendships: [],
+  comments: []
+};
+
+const db = new Low(adapter, defaultData);
+
+module.exports = db;
